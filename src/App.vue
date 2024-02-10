beff1af0635b3cdce516049a8d3f191c09ac2a9a
Jiří Kofroň <kofron.jiri@gmail.com>
2024-02-10 23:25:54 +0100

    base headline styling large displays

--- src/App.vue
+++ src/App.vue
@@ -5,28 +5,36 @@ import { RouterView } from 'vue-router'
 <template>
   <section class="flex flex-col items-center justify-center w-full">
     <section class="flex justify-center w-full bg-primary pt-28">
-      <article class="flex flex-col justify-between w-full max-w-[1000px] h-[380px] text-white pt-16">
-        <div class="flex flex-col flex-nowrap">
+      <article class="flex flex-col justify-between w-full max-w-[1000px] h-[380px] text-white mt-16 px-8 py-5">
+        <div class="flex flex-col flex-nowrap px-3">
           <div class="flex gap-x-7">
             <img src="@/assets/icons/ico-zazemi.svg" alt="ikona hrnicku s srdcem a oblicejem" width="122" height="137"  />
-            <h1 class="text-heading max-w-80 leading-none pt-2 text-white">
-              Poslouchejme děti, když s námi mluví
+            <h1 class="text-heading leading-none pt-2 text-white font-baloo">
+              Poslouchejme děti,
+              <br/>když s námi mluví
             </h1>
           </div>
 
-          <div>
-            <div>
-              Místo, kde vám poradíme co dělat,
-              <span>když dítě není v p o h o d ě</span>
+          <div class="flex justify-end h-28 gap-x-7">
+            <img
+                src="@/assets/icons/ico-orange-arrow.svg"
+                alt="ikona oranžová šipka"
+                width="81"
+                height="142"
+                class="-mt-6"
+            />
+            <div class="flex flex-col justify-center w-full max-w-[480px] px-7 py-2.5 bg-orange rounded-30 gap-y-2.5">
+              <span class="text-xl font-roboto">Místo, kde vám poradíme co dělat,</span>
+              <span class="uppercase text-3xl font-bold font-baloo">když dítě není v p o h o d ě...</span>
             </div>
           </div>
         </div>
 
-        <nav class="flex items-center justify-between">
-          <RouterLink to="/">Představení</RouterLink>
-          <RouterLink to="/varovne-signaly">Varovné signály</RouterLink>
-          <RouterLink to="/jak-vest-rozhovor">Jak s dítětem mluvit</RouterLink>
-          <RouterLink to="/mista-pomoci">Místa pomoci a podpory</RouterLink>
+        <nav class="flex items-center gap-x-2.5 font-montserrat font-semibold">
+          <RouterLink to="/" class="py-2 px-4 rounded-3xl">Představení</RouterLink>
+          <RouterLink to="/varovne-signaly" class="py-2 px-4 rounded-3xl">Varovné signály</RouterLink>
+          <RouterLink to="/jak-vest-rozhovor" class="py-2 px-4 rounded-3xl">Jak s dítětem mluvit</RouterLink>
+          <RouterLink to="/mista-pomoci" class="py-2 px-4 rounded-3xl">Místa pomoci a podpory</RouterLink>
         </nav>
       </article>
     </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,21 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: '#6a498f'
+        primary: '#6A498F',
+        orange: '#E67F4F'
       },
       fontSize: {
         heading: '2.5rem'
+      },
+      borderRadius: {
+        '30': '30px'
       }
     },
+    fontFamily: {
+      baloo: ["Baloo", 'sans-serif'],
+      montserrat: ['Montserrat', 'serif'],
+      roboto: ['Roboto', 'sans-serif']
+    }
   },
   plugins: [],
 }
